033ead262aa2cfa315eebef45406884b012c5de7
Morgan Sherrill <mo.sherrill@outlook.com>
2021-12-22 14:51:43 -0800

    started on the seeds!

--- models/index.js
+++ models/index.js
@@ -6,12 +6,94 @@ const Contacts = require('./Contacts')
 const ContactsInfo = require('./ContactsInfo')
 const User = require('./User')
 
-note.hasMany(calendar, {
-  foreignKey: 'node_id',
-});
 
-calendar.belongsTo(note, {
-  foreignKey: 'note_id',
-});
+// Todo list connection points
+// User.hasMany(Todo, {
+//   foreignKey: 'user_id',
+
+// });
+
+// Todo.belongsTo(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// User.hasMany(TodoItems, {
+//   foreignKey: 'user_id'
+// })
+
+// TodoItems.belongsTo(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// })
+
+// Todo.hasMany(TodoItems, {
+//   foreignKey: 'blog_id'
+// })
+
+// TodoItems.belongsTo(Todo, {
+//   foreignKey: 'blog_id',
+//   onDelete: 'CASCADE'
+// })
+
+// Notes Connection Points
+
+// User.hasMany(Notes, {
+//   foreignKey: 'user_id',
+
+// });
+
+// Notes.belongsTo(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// User.hasMany(NoteItems, {
+//   foreignKey: 'user_id'
+// })
+
+// NoteItems.belongsTo(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// })
+
+// Notes.hasMany(NoteItems, {
+//   foreignKey: 'blog_id'
+// })
+
+// NoteItems.belongsTo(Notes, {
+//   foreignKey: 'blog_id',
+//   onDelete: 'CASCADE'
+// })
+
+//Connecting up the contacts
+
+// User.hasMany(Contacts, {
+//   foreignKey: 'user_id',
+
+// });
+
+// Contacts.belongsTo(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// User.hasMany(ContactsInfo, {
+//   foreignKey: 'user_id'
+// })
+
+// ContactsInfo.belongsTo(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// })
+
+// Contacts.hasMany(ContactsInfo, {
+//   foreignKey: 'blog_id'
+// })
+
+// ContactsInfo.belongsTo(Contacts, {
+//   foreignKey: 'blog_id',
+//   onDelete: 'CASCADE'
+// })
 
 module.exports = { Todos, TodoItems, Notes, NoteItems, Contacts, ContactsInfo, User };
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
@@ -12,7 +12,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
+
 
 const sess = {
     secret: 'Super secret secret',
@@ -33,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
