3ed68ebc93850be22beca5c31dc5dbdde4366f99
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2021-12-24 09:35:20 -0800

    switching ports

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 7001;
+const PORT = process.env.PORT || 3001;
 
 // const hbs = exphbs.create({ helpers });
 const hbs = exphbs.create();
