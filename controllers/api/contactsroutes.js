e826152ed438cc7affc5b7bf1f2c0c39d287243e
Morgan Sherrill <mo.sherrill@outlook.com>
2021-12-30 12:53:11 -0800

    started the framework for an update/delete contact.. I forgot we need those too!

--- controllers/api/contactsroutes.js
+++ controllers/api/contactsroutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Contacts } = require('../../models');
 
-
+// Get all Contacts
 router.get('/', async (req, res) => {
   try {
       const contactsData = await Contacts.findAll(
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 });
 
 
-
+// Post New Contact Associated with Specific User
 router.post('/', async (req, res) => {
   try {
     const newContact = await Contacts.create({
@@ -33,4 +33,30 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Update a Contact
+router.put('/:id', async (req, res) => {
+  try {
+     const contactData = await Contacts.update(
+       {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone,
+        email: req.body.email,
+        notes: req.body.notes,
+        user_id: req.session.user_id,
+       },
+       {
+         where: {
+           id: req.params.id,
+         },
+       }
+     )
+     res.json(contactData);
+   }
+   catch {
+     console.log(err)
+     res.status(500).json(err);
+   }
+ });
+ 
 module.exports = router;
