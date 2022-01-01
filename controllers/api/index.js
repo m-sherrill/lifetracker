ed2a083e10e303252a07fdfcad0d7fb6c1605fe9
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-01-01 13:52:43 -0800

    renamed script.js to note.js also did fetch to add new notes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-
-// const noteRoutes = require('./noteRoutes');
+const noteRoutes = require('./notesRoutes');
 const contactsRoutes = require('./contactsRoutes');
 // const todoRoutes = require('./todoRoutes');
 const userRoutes = require('./userRoutes')
-
+router.use('/notes', noteRoutes)
 // router.use('/noteRoutes', noteRoutes);
 router.use('/contacts', contactsRoutes);
 // router.use('/todos', todoRoutes);
